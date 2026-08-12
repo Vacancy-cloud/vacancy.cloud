7b837146d29ced4c09a6a904b1804426b969f8b1
Vacancy <vacancy.loud@outlook.com>
2026-08-12 17:48:08 +0300

    Add Mapbox env check

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+console.log(
+  'MAPBOX ENV PRESENT:',
+  Boolean(import.meta.env.VITE_MAPBOX_TOKEN)
+);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
